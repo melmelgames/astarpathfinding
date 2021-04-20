3821137a47ccafbada8a648c4b4d9c4df1235524
Mel Rodrigues <melrodriguesgames@gmail.com>
2021-04-19 18:44:08 -0700

    Implement node and grid scripts.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    private Node[,] grid;
+    private float nodeDiameter;
+    private int gridSizeX;
+    private int gridSizeY;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public LayerMask unwalkableMask;
+
+    private void Start(){
+        nodeDiameter = 2 * nodeRadius;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x/nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y/nodeDiameter);
+        CreateGrid();
+
+    }
+
+    private void CreateGrid(){
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right*gridWorldSize.x/2 - Vector3.forward*gridWorldSize.y/2;
+
+        for(int x = 0; x < gridSizeX; x++){
+            for(int y = 0; y < gridSizeY; y++){
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x,y] = new Node(walkable, worldPoint);
+        }
+
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition){
+        float percentX = (worldPosition.x/gridWorldSize.x) + 0.5f;
+        float percentY = (worldPosition.z/gridWorldSize.y) + 0.5f;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        
+        return grid[x,y];
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if(grid != null){
+            foreach(Node n in grid){
+                Gizmos.color = (n.walkable)?Color.white:Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter-0.1f));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    public Vector3 worldPosition;
+    public bool walkable;
+
+    public Node(bool _walkable, Vector3 _worldPosition){
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+    }
+}
